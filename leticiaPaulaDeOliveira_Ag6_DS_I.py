3bd2585e6afc5ca22e8147a683a57f6883a19388
Letícia <leticiapaulade@gmail.com>
2026-09-18 19:46:36 -0300

    Otimização de código.

--- leticiaPaulaDeOliveira_Ag6_DS_I.py
+++ leticiaPaulaDeOliveira_Ag6_DS_I.py
@@ -1,7 +1,7 @@
 # Solicita ao usuário o valor total da compra
 valor_compra = float(input("Digite o valor da compra: R$ "))
 
-# Define o percentual de desconto
+# Verifica qual percentual de desconto deve ser aplicado
 if valor_compra < 200:
     percentual = 0.05
 elif valor_compra < 300:
@@ -9,11 +9,12 @@ elif valor_compra < 300:
 else:
     percentual = 0.15
 
-# Calcula o desconto e o valor total
+# Calcula o valor do desconto
 desconto = valor_compra * percentual
+
+# Calcula o valor total após o desconto
 total = valor_compra - desconto
 
 # Exibe os resultados
-print(f"Seu desconto é de {percentual * 100:.0f}%!")
 print(f"Valor do desconto: R$ {desconto:.2f}")
-print(f"Valor total da compra: R$ {total:.2f}")
+print(f"Valor total a pagar: R$ {total:.2f}")
